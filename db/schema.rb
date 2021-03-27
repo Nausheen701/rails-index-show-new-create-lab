cf0afd3649eec8a72ce5fc171382fa78da4910fc
Nausheen Akhter <nausheen701@gmail.com>
2021-03-27 18:21:38 +0300

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,5 +12,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
-
+    class CreateCoupons < ActiveRecord::Migration
+        create_table "coupons", force: :cascade do |t|
+            t.string   "coupon_code"
+            t.string   "store"
+            t.datetime "created_at",  null: false
+            t.datetime "updated_at",  null: false
+          end
 end
